63cb213bb8d481396ef8d7957070af056a8ad858
hahaxw <2270506143@qq.com>
2021-01-25 09:32:26 +0800

    项目demo

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class DemoApplication {
 
